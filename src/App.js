ad2e76636d177bbb289aed8bca9734b3b8955e9b
danielm2402 <daniel_m2402@hotmail.com>
2022-02-21 14:57:22 -0500

    added ospf & bandwidth

--- src/App.js
+++ src/App.js
@@ -38,24 +38,150 @@ const prefijos = {
   1: '128.0.0.0',
 }
 
+const wildcard = {
+  30: '0.0.0.3',
+  29: '0.0.0.7',
+  28: '0.0.0.15',
+  27: '0.0.0.31',
+  26: '0.0.0.63',
+  25: '0.0.0.127',
+  24: '0.0.0.255',
+  23: '0.0.1.255',
+  22: '0.0.3.255',
+  21: '0.0.7.255',
+  20: '0.0.15.255',
+  19: '0.0.31.255',
+  18: '0.0.63.255',
+  17: '0.0.127.255',
+  16: '0.0.255.255',
+  15: '0.1.255.255',
+  14: '0.3.255.255',
+  13: '0.7.255.255',
+  12: '0.15.255.255',
+  11: '0.31.255.255',
+  10: '0.63.255.255',
+  9: '0.127.255.255',
+  8: '0.255.255.255',
+  7: '1.255.255.255',
+  6: '3.255.255.255',
+  5: '7.255.255.255',
+  4: '15.255.255.255',
+  3: '31.255.255.255',
+  2: '63.255.255.255',
+  1: '127.255.255.255',
+}
+
+
+const convertToBinary1 = (number) => {
+  let num = number;
+  let binary = (num % 2).toString();
+  for (; num > 1;) {
+    num = parseInt(num / 2);
+    binary = (num % 2) + (binary);
+  }
+
+  const countDig = 8 - binary.toString().length
+  let newBi = binary.toString()
+  for (let index = 0; index < countDig; index++) {
+    newBi = '0' + newBi
+  }
+  return newBi
+}
+
+function BinarioADecimal(num) {
+  let sum = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    sum += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum;
+}
+
 
 function App() {
   const [router, setRouter] = useState({ switchs: [] })
   const [rip, setRip] = useState([])
   const [loop, setLoop] = useState([])
   const [cisco, setCisco] = useState({ nombre: '', contrasena: '' })
+  const [eigrp, setEigrp] = useState([])
+  const [sumarizacion, setSumarizacion] = useState([])
+  const [totalSum, setTotalSum] = useState({ ip: [], mascara: '0', binario: [] })
+  const [ospf, setOspf] = useState([])
+
+
+  useEffect(() => {
+    if (sumarizacion.length) {
+      let sum = [...sumarizacion]
+      let bite = 0
+
+      for (let index = 0; index < 32; index++) {
+        const ip = sum[0].binario
+        for (let index1 = 1; index1 < sum.length; index1++) {
+          if (sum[index1].binario[index] !== ip[index]) {
+            console.log(index)
+            bite = index;
+            break;
+          }
+        }
+        if (bite !== 0) {
+          break;
+        }
+      }
+
+      let newSum = [...sum]
+
+      for (let index = 0; index < sum.length; index++) {
+        let binario = [...newSum[index].binario]
+        for (let index1 = 0; index1 < 32; index1++) {
+          if (index1 >= bite) {
+            binario[index1] = '0'
+          }
+        }
+
+        newSum[index] = { ...newSum[index], binario: [...binario] }
+      }
+
+      if (newSum.length) {
+        let sumarizada = [...newSum[0].binario]
+        let finalip = []
+        for (let index = 0; index < sumarizada.length;) {
+          const sum = sumarizada[index].toString() + sumarizada[index + 1].toString() + sumarizada[index + 2].toString() + sumarizada[index + 3].toString() + sumarizada[index + 4].toString() + sumarizada[index + 5].toString() + sumarizada[index + 6].toString() + sumarizada[index + 7].toString()
+          const num = BinarioADecimal(sum)
+          finalip.push(num)
+          index = index + 8
+        }
+
+        setTotalSum({ ip: finalip, binario: sumarizada, mascara: prefijos[bite] })
+      }
+
+
+    }
+  }, [sumarizacion])
 
   const addSwitch = () => {
-    const swi = { interface: '', ip: '', interfaceType: 'gigabitEthernet', prefix: '' }
+    const swi = { interface: '', ip: '', interfaceType: 'gigabitEthernet', prefix: '', bandwidth: null }
     setRouter({ ...router, switchs: [...router.switchs, swi] })
   }
   const addRip = () => {
     const r = { ip: '' }
     setRip([...rip, r])
   }
+  const addEigrp = () => {
+    const e = { ip: '', wildcard: '' }
+    setEigrp([...eigrp, e])
+  }
   const addLoop = () => {
     const l = { ip: '', numero: '', mascara: '' }
     setLoop([...loop, l])
+  }
+  const addOspf = () => {
+    const o = { ip: '', prefix: '' }
+    setOspf([...ospf, o])
+  }
+  const hadlerOspf = (e, i) => {
+    let array = [...ospf]
+    array[i] = { ...array[i], [e.target.name]: e.target.value }
+    setOspf(array)
   }
   const handlerSwitch = (e, i) => {
     let array = [...router.switchs]
@@ -67,10 +193,34 @@ function App() {
     array[i] = { ...array[i], ip: e.target.value }
     setRip(array)
   }
+  const handlerEigrp = (e, i) => {
+    let array = [...eigrp]
+    array[i] = { ...array[i], [e.target.name]: e.target.value }
+    setEigrp(array)
+  }
   const handlerLoop = (e, i) => {
     let array = [...loop]
     array[i] = { ...array[i], [e.target.name]: e.target.value }
     setLoop(array)
+  }
+  const addSumarizacion = () => {
+    const sum = { ip: '', binario: [] }
+    setSumarizacion([...sumarizacion, sum])
+  }
+  const handlerSumarizacion = (e, i) => {
+    let array = [...sumarizacion]
+    const myArray = e.target.value.split(".");
+    const arraybi = myArray.map(element => {
+      return convertToBinary1(element)
+    });
+
+    let a = []
+    for (let index = 0; index < arraybi.length; index++) {
+      const element = arraybi[index];
+      a = [...a, ...arraybi[index]]
+    }
+    array[i] = { ...array[i], binario: a, ip: e.target.value }
+    setSumarizacion(array)
   }
   return (
     <div>
@@ -79,7 +229,9 @@ function App() {
         <button onClick={addSwitch}>+</button>
         <button onClick={addLoop}>LOOPBACK</button>
         <button onClick={addRip}>RIP</button>
-
+        <button onClick={addEigrp}>EIGRP</button>
+        <button onClick={addOspf}> OSPF</button>
+        <button onClick={addSumarizacion}> Sumarización</button>
       </div>
 
       <div style={{ paddingTop: '20px' }}>
@@ -100,6 +252,9 @@ function App() {
 
             <label>Prefix</label>
             <input name='prefix' value={sw.prefix} onChange={(e) => { handlerSwitch(e, i) }}></input>
+            {sw.interfaceType === 'serial' && <>
+              <label>Bandwidth</label>
+              <input name='bandwidth' value={sw.bandwidth} onChange={(e) => { handlerSwitch(e, i) }}></input></>}
           </div>)
         })}
       </div>
@@ -118,10 +273,14 @@ function App() {
                   <code style={{ display: 'block' }}>
                     interface {item.interfaceType} {item.interface}
                   </code>
-                  {item.interfaceType == 'serial' ? <code style={{ display: 'block' }}>clock rate 64000 *</code> : <></>}
                   <code style={{ display: 'block' }}>
                     ip address {item.ip} {prefijos[item.prefix]}
                   </code>
+                  {item.bandwidth&&
+                    <code style={{ display: 'block' }}>
+                      bandwidth {item.bandwidth}
+                    </code>
+                  }
                   <code style={{ display: 'block' }}>
                     no shutdown
                   </code>
@@ -140,6 +299,11 @@ function App() {
                 <code style={{ display: 'block' }}>
                   ip address {item.ip} {prefijos[item.prefix]}
                 </code>
+                {item.bandwidth&&
+                    <code style={{ display: 'block' }}>
+                      bandwidth {item.bandwidth}
+                    </code>
+                  }
                 <code style={{ display: 'block' }}>
                   no shutdown
                 </code>
@@ -151,8 +315,9 @@ function App() {
       </div>
 
       <div style={{ paddingTop: '20px' }}>
-        LOOPBACK
+
         {loop.length > 0 && <div>
+          LOOPBACK
           {loop.map((item, index) => {
             return <div style={{ paddingTop: '20px' }}>
               <label>ip</label>
@@ -183,9 +348,9 @@ function App() {
 
 
       <div style={{ paddingTop: '20px' }}>
-        RIP
         {rip.length > 0 &&
           <div>
+            RIP
             {rip.map((item, index) => {
               return (<div>
                 <label>Network:</label>
@@ -231,45 +396,165 @@ function App() {
 
 
       <div style={{ paddingTop: '20px' }}>
-        ISP
-        <div>
-          <code style={{ display: 'block' }}>
-            ip route "ip calculada" "mascara calculada" "salto"
-          </code>
-          <code style={{ display: 'block' }}>
-            ip route 0.0.0.0 0.0.0.0 "salto"
-          </code>
-          <code style={{ display: 'block' }}>
-            router rip
-          </code>
-          <code style={{ display: 'block' }}>
-            default-information originate
-          </code>
-        </div>
+        {eigrp.length > 0 &&
+          <div>
+            EIGRP
+            {eigrp.map((item, index) => {
+              return (<div>
+                <label>Network:</label>
+                <input value={item.ip} name="ip" onChange={(e) => handlerEigrp(e, index)} />
+                <input value={item.wildcard} name="wildcard" onChange={(e) => handlerEigrp(e, index)} ></input>
+              </div>)
+            })}
+            <div style={{ padding: '20px' }}>
+              <code style={{ display: 'block' }}>
+                enable
+              </code>
+              <code style={{ display: 'block' }}>
+                configure terminal
+              </code>
+              <code style={{ display: 'block' }}>
+                router eigrp 10
+              </code>
+              {eigrp.map(item => {
+                return <code style={{ display: 'block' }}>
+                  network {item.ip} {wildcard[item.wildcard]}
+                </code>
+              })}
+              <code style={{ display: 'block' }}>
+                no auto-summary
+              </code>
+              <div style={{ paddingTop: '20px' }}>
+                {router.switchs.map((item, index) => {
+                  if (item.interfaceType == 'gigabitEthernet') {
+                    return <code style={{ display: 'block' }}>
+                      passive-interface {item.interfaceType} {item.interface}
+                    </code>
+                  }
+                })}
+              </div>
+            </div>
+          </div>
 
+        }
       </div>
 
       <div style={{ paddingTop: '20px' }}>
-        RUTA ESTATICA DE RESUMEN
-        <div>
-          <code style={{ display: 'block' }}>
-            ip route ip mascara null0
-          </code>
-          <code style={{ display: 'block' }}>
-            router rip
-          </code>
-          <code style={{ display: 'block' }}>
-            redistribute static
-          </code>
-        </div>
+        {ospf.length > 0 &&
+          <div>
+            OSPF
+            {ospf.map((item, index) => {
+              return (<div>
+                <label>Network:</label>
+                <input value={item.ip} name="ip" onChange={(e) => hadlerOspf(e, index)} />
+                <label>Prefijo</label>
+                <input value={item.prefix} name="prefix" onChange={(e) => hadlerOspf(e, index)} />
+              </div>)
+            })}
+            <div style={{ padding: '20px' }}>
+              <code style={{ display: 'block' }}>
+                enable
+              </code>
+              <code style={{ display: 'block' }}>
+                configure terminal
+              </code>
+              <code style={{ display: 'block' }}>
+                router ospf 1
+              </code>
+              {ospf.map(item => {
+                return <code style={{ display: 'block' }}>
+                  network {item.ip} {wildcard[item.prefix]} area 0
+                </code>
+
+              })}
+            </div>
+            <div style={{ paddingTop: '20px' }}>
+              {router.switchs.map((item, index) => {
+                if (item.interfaceType == 'gigabitEthernet') {
+                  return <code style={{ display: 'block' }}>
+                    passive-interface {item.interfaceType} {item.interface}
+                  </code>
+                }
+              })}
+            </div>
+          </div>
+
+        }
       </div>
 
+      <div style={{ paddingTop: '20px' }}>
+        {sumarizacion.length > 0 &&
+          <div>
+            SUMARIZACIÓN
+            {sumarizacion.map((item, index) => {
+              return (<div>
+                <label>IP:</label>
+                <input value={item.ip} name="ip" onChange={(e) => handlerSumarizacion(e, index)} />
+              </div>)
+            })}
+
+            <div style={{ padding: '20px' }}>
+              <div>En ISP</div>
+              <code style={{ display: 'block' }}>
+                enable
+              </code>
+              <code style={{ display: 'block' }}>
+                configure terminal
+              </code>
+              <code style={{ display: 'block' }}>
+                ip route {totalSum.ip.toString().replaceAll(",", ".")} {totalSum.mascara} "salto"
+              </code>
+              <div>En router que conecta a ISP</div>
+              <code style={{ display: 'block' }}>
+                ip route 0.0.0.0 0.0.0.0 "salto"
+              </code>
+              {rip.length > 0 ?
+                <div>
+                  <code style={{ display: 'block' }}>
+                    router rip
+                  </code>
+                  <code style={{ display: 'block' }}>
+                    default-information originate
+                  </code>
+                </div> : <></>}
+              {eigrp.length > 0 ?
+                <div>
+                  <code style={{ display: 'block' }}>
+                    router eigrp 10
+                  </code>
+                  <code style={{ display: 'block' }}>
+                    redistribute static
+                  </code>
+                </div> : <></>}
+            </div>
+          </div>
+
+        }
+      </div>
+      {rip.length > 0 ?
+        <div style={{ paddingTop: '20px' }}>
+          RUTA ESTATICA DE RESUMEN
+          <div>
+            <code style={{ display: 'block' }}>
+              ip route ip mascara null0
+            </code>
+            <code style={{ display: 'block' }}>
+              router rip
+            </code>
+            <code style={{ display: 'block' }}>
+              redistribute static
+            </code>
+          </div>
+        </div> : <></>
+      }
+
       <div style={{ marginTop: '120px' }}>
+        <h2>Configuración de nombre y contraseñas</h2>
         <label>nombre</label>
         <input value={cisco.nombre} onChange={(e) => { setCisco({ ...cisco, nombre: e.target.value }) }}></input>
         <label>contraseña</label>
         <input value={cisco.contrasena} onChange={(e) => { setCisco({ ...cisco, contrasena: e.target.value }) }}></input>
-        <div style={{paddingTop:'20px'}}>
+        <div style={{ paddingTop: '20px' }}>
           <code style={{ display: 'block' }}>
             enable
           </code>
@@ -291,10 +576,10 @@ function App() {
           <code style={{ display: 'block' }}>
             login
           </code>
-          <code  style={{ display: 'block' }}>
+          <code style={{ display: 'block' }}>
             exit
           </code>
-          <code  style={{ display: 'block' }}>
+          <code style={{ display: 'block' }}>
             line vty 0 4
           </code>
           <code style={{ display: 'block' }}>
@@ -307,6 +592,14 @@ function App() {
             exit
           </code>
         </div>
+      </div>
+
+      <div style={{ marginTop: '120px' }}>
+        <h2>Resolución de problemas</h2>
+        <code style={{ display: 'block' }}>
+          show ip eigrp neighbors
+        </code>
+
       </div>
     </div >
 
